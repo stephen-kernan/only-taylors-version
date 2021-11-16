d64ce10519c8238d255c67809c0a8c7784c925bf
stephen <kernan.software@gmail.com>
2021-11-15 20:20:31 -0600

    make links open new tab

--- Components/GlobalFooter.js
+++ Components/GlobalFooter.js
@@ -10,7 +10,10 @@ export const GlobalFooter = () => {
           component="p"
           className={styles.footerContent}
         >
-          <Link href="https://www.npr.org/2019/08/22/753393630/look-what-they-made-her-do-taylor-swift-to-re-record-her-catalog">
+          <Link
+            target="_blank"
+            href="https://www.npr.org/2019/08/22/753393630/look-what-they-made-her-do-taylor-swift-to-re-record-her-catalog"
+          >
             Why is Taylor Re-recording?
           </Link>
         </Typography>
@@ -19,7 +22,10 @@ export const GlobalFooter = () => {
           component="p"
           className={styles.footerContent}
         >
-          <Link href="https://www.npr.org/2019/08/22/753393630/look-what-they-made-her-do-taylor-swift-to-re-record-her-catalog">
+          <Link
+            target="_blank"
+            href="https://www.npr.org/2019/08/22/753393630/look-what-they-made-her-do-taylor-swift-to-re-record-her-catalog"
+          >
             View Code
           </Link>
         </Typography>
