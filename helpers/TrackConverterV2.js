c33f14629eb4df06414c770971c3a20e1681228c
Brent Dolan <94938468+brentdolan@users.noreply.github.com>
2023-07-31 20:26:01 -0500

    LTC-19 Did all of the tests (#7)

--- helpers/TrackConverterV2.js
+++ helpers/TrackConverterV2.js
@@ -1,99 +1,371 @@
+import axios from 'axios'
 import {
-    addTracksToPlaylist,
-    deleteTracksInPlaylist,
-    fetchCurrentUserID, fetchTracksInPlaylist,
-    fetchUserPlaylists, findOldTracks,
-    replaceTracksInPlaylist,
-    replaceUserSavedAlbums,
-    replaceUserSavedTracks
-} from "./trackConverter";
+  addTracksToPlaylist,
+  deleteTracksInPlaylist,
+  fetchCurrentUserID, fetchTracksInPlaylist,
+  fetchUserPlaylists, findOldAlbums, findOldTracks, removeUserSavedAlbums,
+  replaceTracksInPlaylist,
+  replaceUserSavedAlbums,
+  replaceUserSavedTracks, saveUserAlbums,
+  requestHeaders, saveUserTracks, removeUserSavedTracks, handleErrors
+} from './trackConverter'
+import { alternativeVersions, conversionMap } from './conversionMap'
 
 export class TrackConverterV2 {
-    token = ""
-    tenMinuteVersion = false
-    numberOfTracksUpdated = 0
-    numberOfPlaylistsUpdated = 0
-    constructor(token, tenMinuteVersion) {
-        this.token = token
-        this.tenMinuteVersion = tenMinuteVersion
+  token = ''
+
+  tenMinuteVersion = false
+
+  numberOfTracksUpdated = 0
+
+  numberOfPlaylistsUpdated = 0
+
+  constructor (token, tenMinuteVersion) {
+    this.token = token
+    this.tenMinuteVersion = tenMinuteVersion
+  }
+
+  async replaceWithTaylorsVersion () {
+    try {
+      const userID = await this.fetchCurrentUserID(this.token)
+      const playlists = await this.fetchUserPlaylists(this.token, userID)
+
+      // Call the replace function on every playlist async, stacking up the promises
+      const apiCalls = []
+      for (const playlist of playlists) {
+        if (playlist.tracks.total === 0) {
+          continue
+        }
+
+        apiCalls.push(this.replaceTracksInPlaylist(this.token, playlist, this.tenMinuteVersion))
+      }
+
+      // Wait until all API calls are returned so we have the number of tracks
+      const responses = await Promise.all(
+        [
+          ...apiCalls,
+          this.replaceUserSavedTracks(this.token, this.tenMinuteVersion),
+          this.replaceUserSavedAlbums(this.token, this.tenMinuteVersion)
+        ]
+      )
+
+      // Count up all the tracks/playlists
+      responses.forEach(([tracks, playlists]) => {
+        this.numberOfTracksUpdated += tracks
+        this.numberOfPlaylistsUpdated += playlists
+      })
+
+      return `/thank-you?tracks=${this.numberOfTracksUpdated}&playlists=${this.numberOfPlaylistsUpdated}`
+    } catch (err) {
+      console.error(err)
+      return '/uh-oh'
+    }
+  }
+
+  async replaceTracksInPlaylist (playlist) {
+    let numberOfTracksUpdated = 0
+
+    const tracksInPlaylist = await this.fetchTracksInPlaylist(
+      this.token,
+      playlist.tracks.href,
+      playlist.tracks.total
+    )
+
+    const [tracksToReplace, tracksToAdd] = await this.findOldTracks(
+      tracksInPlaylist,
+      false,
+      this.tenMinuteVersion
+    )
+
+    if (!tracksToReplace.length) {
+      return [0, 0]
     }
 
-    async replaceWithTaylorsVersion() {
-        try {
-            const userID = await fetchCurrentUserID(this.token)
-            const playlists = await fetchUserPlaylists(this.token, userID);
+    await this.deleteTracksInPlaylist(
+      this.token,
+      playlist.id,
+      tracksToReplace.length,
+      tracksToReplace
+    )
 
-            // Call the replace function on every playlist async, stacking up the promises
-            const apiCalls = []
-            for (const playlist of playlists) {
-                if (playlist.tracks.total === 0) {
-                    continue;
-                }
+    await this.addTracksToPlaylist(
+      this.token,
+      playlist.id,
+      tracksToAdd.length,
+      tracksToAdd
+    )
 
-                apiCalls.push(replaceTracksInPlaylist(this.token, playlist, this.tenMinuteVersion))
-            }
-
-            // Wait until all API calls are returned so we have the number of tracks
-            const responses = await Promise.all(
-                [
-                    ...apiCalls,
-                    replaceUserSavedTracks(this.token, this.tenMinuteVersion),
-                    replaceUserSavedAlbums(this.token, this.tenMinuteVersion)
-                ]
-            )
-
-            // Count up all the tracks/playlists
-            responses.map(([tracks, playlists]) => {
-                this.numberOfTracksUpdated += tracks
-                this.numberOfPlaylistsUpdated += playlists
-            })
-
-            return `/thank-you?tracks=${this.numberOfTracksUpdated}&playlists=${this.numberOfPlaylistsUpdated}`;
-        } catch (err) {
-            console.error(err);
-            return `/uh-oh`;
-        }
-    };
-    async replaceTracksInPlaylist(playlist) {
-        let numberOfTracksUpdated = 0;
-
-        const tracksInPlaylist = await fetchTracksInPlaylist(
-            this.token,
-            playlist.tracks.href,
-            playlist.tracks.total
-        );
-
-        const [tracksToReplace, tracksToAdd] = await findOldTracks(
-            tracksInPlaylist,
-            false,
-            this.tenMinuteVersion
-        );
-
-        if (!tracksToReplace.length) {
-            return [0, 0]
-        }
-
-        await deleteTracksInPlaylist(
-            this.token,
-            playlist.id,
-            tracksToReplace.length,
-            tracksToReplace
-        );
-
-        await addTracksToPlaylist(
-            this.token,
-            playlist.id,
-            tracksToAdd.length,
-            tracksToAdd
-        );
-
-        if (tracksToReplace.length) {
-            numberOfTracksUpdated = numberOfTracksUpdated + tracksToReplace.length;
-        }
-
-        return [numberOfTracksUpdated, 1]
+    if (tracksToReplace.length) {
+      numberOfTracksUpdated += tracksToReplace.length
     }
 
+    return [numberOfTracksUpdated, 1]
+  }
 
+  async replaceUserSavedAlbums () {
+    const userSavedAlbums = []
+    await axios.get(
+      'https://api.spotify.com/v1/me/albums',
+      { headers: this.requestHeaders(this.token) }
+    ).then(({ data }) => {
+      userSavedAlbums.push(...data.items)
+    })
+
+    const [albumsToReplace, albumsToAdd, totalTracksReplaced] = await this.findOldAlbums(
+      userSavedAlbums
+    )
+
+    await this.saveUserAlbums(this.token, albumsToAdd)
+    await this.removeUserSavedAlbums(this.token, albumsToReplace)
+
+    // Return zero since these technically aren't in a playlist
+    return [totalTracksReplaced, 0]
+  }
+
+  async replaceUserSavedTracks () {
+    const userSavedTracks = []
+    await axios.get(
+      'https://api.spotify.com/v1/me/tracks',
+      { headers: this.requestHeaders(this.token) }
+    ).then(({ data }) => {
+      userSavedTracks.push(...data.items)
+    })
+
+    const [tracksToReplace, tracksToAdd] = await this.findOldTracks(userSavedTracks, true)
+
+    await this.saveUserTracks(this.token, tracksToAdd.length, tracksToAdd)
+    await this.removeUserSavedTracks(this.token, tracksToReplace.length, tracksToReplace)
+
+    // Return zero since these technically aren't in a playlist
+    return [tracksToReplace.length, 0]
+  }
+
+  async findOldAlbums (albums = []) {
+    const albumsToReplace = []
+    const albumsToAdd = []
+    let totalTracksToReplace = 0
+
+    for (const album of albums) {
+      if (conversionMap[album?.album?.id]) {
+        albumsToReplace.push(album.album.id)
+        albumsToAdd.push(conversionMap[album.album.id].taylorsVersionId)
+        totalTracksToReplace += album.album.total_tracks
+      }
+    }
+
+    return [albumsToReplace, albumsToAdd, totalTracksToReplace]
+  }
+
+  async findOldTracks (tracksInPlaylist = [], idsOnly = false) {
+    const tracksToReplace = []
+    const tracksToAdd = []
+
+    for (const track of tracksInPlaylist) {
+      const oldTrackId = track?.track?.id
+      const trackIsAllTooWell = oldTrackId === '1q3RiD1tIWUpGsNFADMlvl'
+      if (conversionMap[oldTrackId]) {
+        const newTrackId = (trackIsAllTooWell && this.tenMinuteVersion)
+          ? alternativeVersions[oldTrackId].taylorsVersionId
+          : conversionMap[oldTrackId].taylorsVersionId
+        if (this.tenMinuteVersion && trackIsAllTooWell) {
+          console.log('YES TAYLOR => ', newTrackId)
+        }
+        if (idsOnly) {
+          tracksToReplace.push(oldTrackId)
+          tracksToAdd.push(newTrackId)
+        } else {
+          tracksToReplace.push({
+            uri: `spotify:track:${oldTrackId}`
+          })
+          tracksToAdd.push(
+            `spotify:track:${newTrackId}`
+          )
+        }
+      }
+    }
+
+    return [tracksToReplace, tracksToAdd]
+  }
+
+  async removeUserSavedAlbums (albumIds) {
+    if (!albumIds || !albumIds.length) return []
+
+    await axios
+      .delete('https://api.spotify.com/v1/me/albums', {
+        headers: this.requestHeaders(this.token),
+        data: { ids: albumIds }
+      })
+      .catch((err) => {
+        this.handleErrors(err)
+      })
+  }
+
+  async saveUserAlbums (albumIds) {
+    if (!albumIds || !albumIds.length) return []
+
+    await axios
+      .put(
+        'https://api.spotify.com/v1/me/albums',
+        { ids: albumIds },
+        {
+          headers: this.requestHeaders(this.token)
+        }
+      )
+      .catch((err) => {
+        this.handleErrors(err)
+      })
+  }
+
+  async removeUserSavedTracks (total, trackIds) {
+    if (!trackIds || !trackIds.length) return []
+
+    for (let i = 0; i <= total; i += 50) {
+      // i through i + 99 = 100 items. number 100 will be hit on next round
+      const tracksToDelete = trackIds.slice(i, i + 49)
+      await axios
+        .delete('https://api.spotify.com/v1/me/tracks', {
+          headers: this.requestHeaders(this.token),
+          data: { ids: tracksToDelete }
+        })
+        .catch((err) => {
+          this.handleErrors(err)
+        })
+    }
+  }
+
+  async saveUserTracks (total, trackIds) {
+    if (!trackIds || !trackIds.length) return []
+
+    for (let i = 0; i <= total; i += 50) {
+      // i through i + 99 = 100 items. number 100 will be hit on next round
+      const tracksToAdd = trackIds.slice(i, i + 49)
+      await axios
+        .put(
+          'https://api.spotify.com/v1/me/tracks',
+          { ids: tracksToAdd },
+          {
+            headers: this.requestHeaders(this.token)
+          }
+        )
+        .catch((err) => {
+          this.handleErrors(err)
+        })
+    }
+  }
+
+  async deleteTracksInPlaylist (playlistId, total, trackIds) {
+    if (!trackIds || !trackIds.length) {
+      return []
+    }
+    for (let i = 0; i <= total; i += 100) {
+      // i through i + 99 = 100 items. number 100 will be hit on next round
+      const tracksToDelete = trackIds.slice(i, i + 99)
+      await axios
+        .delete(`https://api.spotify.com/v1/playlists/${playlistId}/tracks`, {
+          headers: this.requestHeaders(this.token),
+          data: { tracks: tracksToDelete }
+        })
+        .then(({ data }) => {
+          console.log(data)
+        })
+        .catch((err) => {
+          this.handleErrors(err)
+        })
+    }
+  }
+
+  async addTracksToPlaylist (playlistId, total, trackIds) {
+    if (!trackIds || !trackIds.length) {
+      return []
+    }
+    for (let i = 0; i <= total; i += 100) {
+      // i through i + 99 = 100 items. number 100 will be hit on next round
+      const tracksToAdd = trackIds.slice(i, i + 99)
+      await axios
+        .post(
+          `https://api.spotify.com/v1/playlists/${playlistId}/tracks`,
+          {
+            uris: tracksToAdd
+          },
+          {
+            headers: this.requestHeaders(this.token)
+          }
+        )
+        .then(({ data }) => {
+          console.log(data)
+        })
+        .catch((err) => {
+          this.handleErrors(err)
+        })
+    }
+  }
+
+  async fetchTracksInPlaylist (url, total) {
+    const tracksInPlaylist = []
+    for (let i = 0; i <= total; i += 100) {
+      await axios
+        .get(`${url}?offset=${i}`, { headers: this.requestHeaders(this.token) })
+        .then(({ data }) => {
+          tracksInPlaylist.push(...data.items)
+        })
+        .catch((err) => {
+          this.handleErrors(err)
+        })
+    }
+    return tracksInPlaylist
+  }
+
+  async fetchCurrentUserID () {
+    return await axios
+      .get('https://api.spotify.com/v1/me', {
+        headers: this.requestHeaders(this.token)
+      })
+      .then(({ data }) => data.id)
+  }
+
+  async fetchUserPlaylists (userID) {
+    const [playlists, total] = await axios
+      .get(`https://api.spotify.com/v1/users/${userID}/playlists`, {
+        headers: this.requestHeaders(this.token)
+      })
+      .then(({ data }) => [data.items, data.total])
+
+    while (playlists.length < total) {
+      await axios
+        .get(
+          `https://api.spotify.com/v1/users/${userID}/playlists?offset=${
+            playlists.length - 1
+          }`,
+          {
+            headers: this.requestHeaders(this.token)
+          }
+        )
+        .then(({ data }) => {
+          playlists.push(...data.items)
+        })
+        .catch((err) => {
+          this.handleErrors(err)
+        })
+    }
+
+    return playlists
+  }
+
+  handleErrors (err) {
+    if (err.response) {
+      const { data, status, headers } = err.response
+
+      if (status === 429) {
+        console.warn('Rate limit exceeded')
+        return
+      }
+
+      console.error(`{ status: ${status}, data: ${data}, headers: ${headers}}`)
+    }
+  }
+
+  requestHeaders = () => ({
+    Authorization: `Bearer ${this.token}`,
+    'Content-Type': 'application/json'
+  })
 }
-
--- tests/helpers/TrackConverterV2.test.js
+++ tests/helpers/TrackConverterV2.test.js
@@ -1,61 +1,245 @@
 import React from 'react'
-import { TrackConverterV2 } from "../../helpers/TrackConverterV2";
-import * as originalFunctions from "../../helpers/trackConverter"
+import axios from 'axios'
+import { TrackConverterV2 } from '../../helpers/TrackConverterV2'
 
-describe('TrackConverterV2',  () => {
-    describe('replaceWithTaylorsVersion()', () => {
-        jest.spyOn(originalFunctions, 'fetchCurrentUserID').mockReturnValue("example")
-        jest.spyOn(originalFunctions, 'fetchUserPlaylists').mockReturnValue([])
-        jest.spyOn(originalFunctions, 'replaceTracksInPlaylist').mockReturnValue([0,0])
-        const replaceSavedAlbumSpy = jest.spyOn(originalFunctions, 'replaceUserSavedAlbums').mockReturnValue([0,0])
-        jest.spyOn(originalFunctions, 'replaceUserSavedTracks').mockReturnValue([0,0])
+describe('TrackConverterV2', () => {
+  describe('replaceWithTaylorsVersion()', () => {
+    const converter = new TrackConverterV2('', false)
+    converter.fetchCurrentUserID = () => ('example')
+    converter.fetchUserPlaylists = () => ([])
+    converter.replaceTracksInPlaylist = () => ([0, 0])
+    converter.replaceUserSavedAlbums = () => ([0, 0])
+    converter.replaceUserSavedTracks = () => ([0, 0])
 
+    it('Returns URL with tracks and playlist filled out', async () => {
+      converter.replaceUserSavedAlbums = () => ([2, 10])
 
-        it('Returns URL with tracks and playlist filled out', async () => {
-            replaceSavedAlbumSpy.mockReturnValue([2,10])
-            const converter = new TrackConverterV2('', false)
-
-            const response = await converter.replaceWithTaylorsVersion()
-            const expected =  `/thank-you?tracks=2&playlists=10`
-            expect(response).toBe(expected)
-        })
-
-        it('Returns uh oh if fails', async () => {
-            replaceSavedAlbumSpy.mockImplementation(() => {
-                throw new Error()
-            })
-            const converter = new TrackConverterV2('', false)
-
-            const response = await converter.replaceWithTaylorsVersion()
-            const expected =  `/uh-oh`
-            expect(response).toBe(expected)
-        })
+      const response = await converter.replaceWithTaylorsVersion()
+      const expected = '/thank-you?tracks=2&playlists=10'
+      expect(response).toBe(expected)
     })
 
-    describe('replaceTracksInPlaylist()', () => {
-        jest.spyOn(originalFunctions, 'fetchTracksInPlaylist').mockReturnValue([])
-        jest.spyOn(originalFunctions, 'deleteTracksInPlaylist')
-        jest.spyOn(originalFunctions, 'addTracksToPlaylist')
-        const findOldTracksSpy = jest.spyOn(originalFunctions, 'findOldTracks').mockReturnValue([[],[]])
+    it('If fails returns uh oh', async () => {
+      converter.replaceUserSavedAlbums = () => {
+        throw new Error()
+      }
 
+      const response = await converter.replaceWithTaylorsVersion()
+      const expected = '/uh-oh'
+      expect(response).toBe(expected)
+    })
+  })
 
-        it('If tracks replaced, returns number of tracks and playlists updated', async () => {
-            findOldTracksSpy.mockReturnValue([[76,66],[12]])
-            const converter = new TrackConverterV2('', false)
+  describe('replaceTracksInPlaylist()', () => {
+    const converter = new TrackConverterV2('', false)
+    converter.fetchTracksInPlaylist = () => ([])
+    converter.deleteTracksInPlaylist = () => {}
+    converter.addTracksToPlaylist = () => {}
+    converter.findOldTracks = () => ([[], []])
 
-            const response = await converter.replaceTracksInPlaylist({tracks:{href:0,total:0}})
-            const expected =  [2,1]
-            expect(response).toEqual(expected)
-        })
+    it('If tracks replaced, returns number of tracks and playlists updated', async () => {
+      converter.findOldTracks = () => ([[76, 66], [12]])
 
-        it('If not tracks to replace, returns 0 and 0', async () => {
-            findOldTracksSpy.mockReturnValue([[],[]])
-            const converter = new TrackConverterV2('', false)
-
-            const response = await converter.replaceTracksInPlaylist({tracks:{href:0,total:0}})
-            const expected =  [0,0]
-            expect(response).toEqual(expected)
-        })
+      const response = await converter.replaceTracksInPlaylist({ tracks: { href: 0, total: 0 } })
+      const expected = [2, 1]
+      expect(response).toEqual(expected)
     })
 
+    it('If not tracks to replace, returns 0 and 0', async () => {
+      const converter = new TrackConverterV2('', false)
+      converter.findOldTracks = () => ([[], []])
+
+      const response = await converter.replaceTracksInPlaylist({ tracks: { href: 0, total: 0 } })
+      const expected = [0, 0]
+      expect(response).toEqual(expected)
+    })
+  })
+
+  describe('replaceUserSavedTracks()', () => {
+    const converter = new TrackConverterV2('', false)
+    beforeAll(() => {
+      converter.removeUserSavedTracks = () => {}
+      converter.saveUserTracks = () => {}
+      converter.findOldTracks = () => ([[], []])
+      jest.spyOn(axios, 'get').mockImplementation(() => Promise.resolve({ data: { items: [] } }))
+      jest.spyOn(axios, 'put').mockReturnValue()
+      jest.spyOn(axios, 'delete').mockReturnValue()
+    })
+
+    it('Returns number of tracks and playlists updated', async () => {
+      converter.findOldTracks = () => ([[2, 22], []])
+
+      const response = await converter.replaceUserSavedTracks({ tracks: { href: 0, total: 0 } })
+      const expected = [2, 0]
+      expect(response).toEqual(expected)
+    })
+  })
+
+  describe('replaceUserSavedAlbums()', () => {
+    beforeAll(() => {
+      jest.spyOn(axios, 'get').mockImplementation(() => Promise.resolve({ data: { items: [] } }))
+    })
+
+    it('Returns number of tracks and playlists updated', async () => {
+      const converter = new TrackConverterV2('', false)
+      converter.findOldAlbums = () => ([[2, 22], [87], 5])
+      converter.removeUserSavedAlbums = () => {}
+      converter.saveUserAlbums = () => {}
+
+      const response = await converter.replaceUserSavedAlbums()
+      const expected = [5, 0]
+      expect(response).toEqual(expected)
+    })
+  })
+
+  describe('findOldAlbums()', () => {
+    it('Returns albumsToReplace, albumsToAdd, totalTracksToReplace', async () => {
+      const converter = new TrackConverterV2('', false)
+
+      const response = await converter.findOldAlbums([{ album: { id: '1OYARuagDrpgNNQ4loO1Cs', total_tracks: 33 } }])
+      const expected = [['1OYARuagDrpgNNQ4loO1Cs'], ['5YqltLsjdqFtvqE7Nrysvs'], 33]
+      expect(response).toEqual(expected)
+    })
+  })
+
+  describe('findOldTracks()', () => {
+    it('Returns tracksToReplace and tracksToAdd', async () => {
+      const converter = new TrackConverterV2('', false)
+
+      const response = await converter.findOldTracks([{ track: { id: '1OYARuagDrpgNNQ4loO1Cs' } }])
+      const expected = [[{
+        uri: 'spotify:track:1OYARuagDrpgNNQ4loO1Cs'
+      }], ['spotify:track:5YqltLsjdqFtvqE7Nrysvs']]
+      expect(response).toEqual(expected)
+    })
+  })
+
+  describe('saveUserAlbums()', () => {
+    it('Calls axios.put', async () => {
+      const putSpy = jest.spyOn(axios, 'put').mockImplementation(() => Promise.resolve('test'))
+      const converter = new TrackConverterV2('', false)
+
+      await converter.saveUserAlbums([2])
+      expect(putSpy).toBeCalled()
+    })
+    it('If not albumIds or if not albumIds.length return nothing', async () => {
+      const converter = new TrackConverterV2('', false)
+
+      const response = await converter.saveUserAlbums([])
+      expect(response).toEqual([])
+    })
+  })
+
+  describe('removeUserSavedAlbums()', () => {
+    it('Calls axios.delete', async () => {
+      const deleteSpy = jest.spyOn(axios, 'delete').mockImplementation(() => Promise.resolve('test'))
+      const converter = new TrackConverterV2('', false)
+
+      await converter.removeUserSavedAlbums([2])
+      expect(deleteSpy).toBeCalled()
+    })
+    it('If not albumIds or if not albumIds.length return nothing', async () => {
+      const converter = new TrackConverterV2('', false)
+
+      const response = await converter.removeUserSavedAlbums([])
+      expect(response).toEqual([])
+    })
+  })
+
+  describe('removeUserSavedTracks()', () => {
+    it('Calls axios.delete', async () => {
+      const deleteSpy = jest.spyOn(axios, 'delete').mockImplementation(() => Promise.resolve('test'))
+      const converter = new TrackConverterV2('', false)
+
+      await converter.removeUserSavedTracks(88, [65741])
+      expect(deleteSpy).toBeCalled()
+    })
+    it('If not albumIds or if not albumIds.length return nothing', async () => {
+      const converter = new TrackConverterV2('', false)
+
+      const response = await converter.removeUserSavedTracks(88, [])
+      expect(response).toEqual([])
+    })
+  })
+
+  describe('saveUserTracks()', () => {
+    it('Calls axios.put', async () => {
+      const putSpy = jest.spyOn(axios, 'put').mockImplementation(() => Promise.resolve('test'))
+      const converter = new TrackConverterV2('', false)
+
+      await converter.saveUserTracks(2, [22])
+      expect(putSpy).toBeCalled()
+    })
+    it('If not albumIds or if not albumIds.length return nothing', async () => {
+      const converter = new TrackConverterV2('', false)
+
+      const response = await converter.saveUserTracks(897, [])
+      expect(response).toEqual([])
+    })
+  })
+
+  describe('deleteTracksInPlaylist()', () => {
+    it('Calls axios.delete', async () => {
+      const deleteSpy = jest.spyOn(axios, 'delete').mockImplementation(() => Promise.resolve('test'))
+      const converter = new TrackConverterV2('', false)
+
+      await converter.deleteTracksInPlaylist('reindeerMan', 88, [65741])
+      expect(deleteSpy).toBeCalled()
+    })
+    it('If not albumIds or if not albumIds.length return nothing', async () => {
+      const converter = new TrackConverterV2('', false)
+
+      const response = await converter.deleteTracksInPlaylist('reindeerMan', 88, [])
+      expect(response).toEqual([])
+    })
+  })
+
+  describe('addTracksToPlaylist()', () => {
+    const postSpy = jest.spyOn(axios, 'post').mockImplementation(() => Promise.resolve('test'))
+    it('Calls axios.post', async () => {
+      const converter = new TrackConverterV2('', false)
+
+      await converter.addTracksToPlaylist('reindeerMan', 88, [65741])
+      expect(postSpy).toBeCalled()
+    })
+    it('If not albumIds or if not albumIds.length return nothing', async () => {
+      const converter = new TrackConverterV2('', false)
+
+      const response = await converter.addTracksToPlaylist('reindeerMan', 88, [])
+      expect(response).toEqual([])
+    })
+  })
+
+  describe('fetchTracksInPlaylist()', () => {
+    const getSpy = jest.spyOn(axios, 'get').mockImplementation(() => Promise.resolve('test'))
+    it('Calls axios.get', async () => {
+      const converter = new TrackConverterV2('', false)
+
+      await converter.fetchTracksInPlaylist('reindeerMan', 88)
+      expect(getSpy).toBeCalled()
+    })
+  })
+
+  describe('fetchCurrentUserID()', () => {
+    beforeAll(() => {
+      jest.spyOn(axios, 'get').mockImplementation(() => Promise.resolve({ data: { id: 5 } }))
+    })
+
+    it('returns userID', async () => {
+      const converter = new TrackConverterV2('', false)
+      expect(await converter.fetchCurrentUserID()).toEqual(5)
+    })
+  })
+
+  describe('fetchUserPlaylists()', () => {
+    beforeAll(() => {
+      jest.spyOn(axios, 'get').mockImplementation(() => Promise.resolve({ data: { items: ['playlist1', 'playlist2'], total: 4 } }))
+    })
+
+    it('Calls axios.get', async () => {
+      const converter = new TrackConverterV2('', false)
+      const expectedValue = ['playlist1', 'playlist2', 'playlist1', 'playlist2']
+      expect(await converter.fetchUserPlaylists('reindeerMan')).toEqual(expectedValue)
+    })
+  })
 })
